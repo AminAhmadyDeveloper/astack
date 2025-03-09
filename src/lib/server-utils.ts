d36dc4b4aef740a474dd8961242264db4014fff1
Amin Ahmady <aminahmadydeveloper@gmail.com>
2025-03-09 00:17:07 -0800

    fix: server address on production

--- src/lib/server-utils.ts
+++ src/lib/server-utils.ts
@@ -9,6 +9,9 @@ export const getUrl = (suffix?: string[]) => {
 
 export const getWorkerUrl = (suffix?: string[]) => {
   const base = (() => {
+    if (process.env.NODE_ENV === 'production') {
+      return 'astack.aminahmadydeveloper.workers.dev';
+    }
     return 'http://localhost:8080/api';
   })();
   return suffix?.length ? `${base}/${suffix.join('/')}` : base;
